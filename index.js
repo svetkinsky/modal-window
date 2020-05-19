380716d35e0b67f96144273d81208bc7929eef7a
svetkinsky <tebsv@inbox.ru>
2020-05-19 18:58:24 +0300

    create, close, passing options in object

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// const myModal = $.modal()
+const content = `<p>Lorem ipsum dolor sit.</p>
+                <p>Lorem ipsum dolor sit.</p>`
+const options = {
+    title: 'My modal title',
+    closable: true,
+    content: content,
+    modalWidth: '400px'
+}
+const modal = $.modal(options)
--- plugins/modal.js
+++ plugins/modal.js
@@ -1,16 +1,17 @@
 function _createModal(options) {
+    const DEFAULT_WIDTH = '600px'
     const modal = document.createElement('div')
+    const modalCloseElement = options.closable ? `<span class="modal-close">&times;</span>` : ''
     modal.classList.add('vmodal')
     modal.insertAdjacentHTML('afterbegin', `
     <div class="modal-overlay">
-        <div class="modal-window">
+        <div class="modal-window" style = "width: ${options.modalWidth || DEFAULT_WIDTH}">
             <div class="modal-header">
-                <span class="modal-title">Modal title</span>
-                <span class="modal-close">&times;</span>
+                <span class="modal-title">${options.title || 'Modal window'}</span>
+                ${modalCloseElement}
             </div>
             <div class="modal-body">
-                <p>Lorem ipsum dolor sit.</p>
-                <p>Lorem ipsum dolor sit.</p>
+                ${options.content || ''}
             </div>
             <div class="modal-footer">
                 <button>Ok</button>
@@ -19,15 +20,45 @@ function _createModal(options) {
         </div>
     </div>
     `)
+
+
     document.body.appendChild(modal)
     return modal
 }
+
+
+
 $.modal = function(options) {
+    const ANIMATION_SPEED = 200
     const $modal = _createModal(options)
+    let closing = false
+
     return {
-        open() {},
-        close() {},
-        destroy() {}
+        open() {
+            !closing && $modal.classList.add('open')
+            const moladClose = $modal.querySelector('.modal-close')
+            const modalOverlay = $modal.querySelector('.modal-overlay')
+            document.body.addEventListener('click', event => {
+                const target = event.target
+                console.log(event)
+                if (target.className === 'modal-close' || target.className === 'modal-overlay') {
+                    this.close()
+                }
+            })
+        },
+        close() {
+            closing = true
+            $modal.classList.add('hide')
+            setTimeout(() => {
+                $modal.classList.remove('open')
+                closing = false
+            }, ANIMATION_SPEED)
+        },
+        destroy() {
+
+        }
     }
 }
+
+
 
